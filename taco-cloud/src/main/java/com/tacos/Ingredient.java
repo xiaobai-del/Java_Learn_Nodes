4215f97b209c288e40f5a6b345cc0514f0734c7d
CJH <1377174469@qq.com>
2020-11-02 18:25:38 +0800

    demo

--- taco-cloud/src/main/java/com/tacos/Ingredient.java
+++ taco-cloud/src/main/java/com/tacos/Ingredient.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor//这个注解可以生成带参或者不带参的构造方法。
                         //若带参数，只能是类中所有带有 @NonNull注解的和以final修饰的未经初始化的字段
-                        //两个条件都不满足，则生成无参构造方法
+                        //两个条件都不满足，则生成无参构造方法//
 public class Ingredient {
     private final String id;
     private final String name;
